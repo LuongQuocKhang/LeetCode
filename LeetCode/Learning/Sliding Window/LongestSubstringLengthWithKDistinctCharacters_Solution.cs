c1df803f3059d281184b73523952ab446b041def
O2F-IT\luong.quockhang_amar <luong.quockhang@amaris.com>
2023-11-14 14:33:17 +0700

    LongestSubstringLengthWithKDistinctCharacters

---
+++ LeetCode/Learning/Sliding Window/LongestSubstringLengthWithKDistinctCharacters_Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace LeetCode.Learning.Sliding_Window;
+
+public class LongestSubstringLengthWithKDistinctCharacters_Solution
+{
+    public static void Run()
+    {
+        Console.WriteLine(LongestSubstringLengthWithKDistinctCharacters("AAAHHIBC", 4));
+    }
+
+    public static int LongestSubstringLengthWithKDistinctCharacters(string s, int k)
+    {
+        Dictionary<char, int> dict = new();
+
+        string result = "";
+        int start = 0;
+
+        foreach (char c in s)
+        {
+            result += c;
+
+            if (dict.ContainsKey(c))
+            {
+                dict[c] += 1;
+            }
+            else
+            {
+                dict[c] = 1;
+            }
+
+            while(dict.Keys.Count > k)
+            {
+                char rc = result[start];
+
+                result = result.Remove(start, 1);
+                dict[rc] -= 1;
+
+                if (dict[rc] == 0)
+                {
+                    dict.Remove(rc);
+                }
+            }
+        }
+        
+        return result.Length;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,10 @@
 ﻿using LeetCode.Learning.Sliding_Window;
-using LeetCode.Learning.Sliding_Window.Easy;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        MaxSumFixedSizeSubArray_Solution.Run();
+        LongestSubstringLengthWithKDistinctCharacters_Solution.Run();
 
         Console.ReadKey();
     }
