9f071669fcee1eb6f975ff38212d03e3de113e8d
O2F-IT\luong.quockhang_amar <luong.quockhang@amaris.com>
2023-11-14 17:02:43 +0700

    Update

---
+++ LeetCode/Sliding Window/Easy/LongestNiceSubstring_Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Sliding_Window.Easy;
+
+public class LongestNiceSubstring_Solution
+{
+    public static void Run()
+    {
+        Console.WriteLine(LongestNiceSubstring("YazaAay"));
+    }
+
+    public static string LongestNiceSubstring(string s)
+    {
+        List<string> list = new List<string>();
+
+        string result = "";
+
+        for (int end = 0; end < s.Length; end++)
+        {
+            result += s[end];
+
+
+        }
+
+        return result;
+    }
+}
