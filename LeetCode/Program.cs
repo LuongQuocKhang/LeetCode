6f181c8df5bb230880f0a7ab0647062edba17876
LuongQuocKhang <KhangHuu97@gmail.com>
2024-07-08 17:43:13 +0700

    FinalStringSolution

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        FindTheWinnerOfTheCircularGame.Run();
+        FinalStringSolution.Run();
 
         // Console.ReadKey();
     }
---
+++ LeetCode/String/Easy/FinalStringSolution.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace LeetCode.String.Easy;
+
+public class FinalStringSolution
+{
+    /***
+    * Tag: Easy
+    * Name: 2810. Faulty Keyboard
+    * Start Time: 08/07/2024 17:40 AM
+    * End Time: 08/07/2024 17:42 PM
+    * Result: 
+        * Runtime: solution 1: Beats 37.96% of users with C# ( 72ms )
+        * Memory:  solution 1: Beats 90.74% of users with C# ( 50.35MB )
+    ***/
+
+    public static void Run()
+    {
+        Console.WriteLine(FinalString("string"));
+    }
+
+    public static string FinalString(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in s)
+        {
+            if(c == 'i')
+            {
+                int left = 0;
+                int right = sb.Length - 1;
+                while(left < right)
+                {
+                    char temp = sb[left];
+                    sb[left] = sb[right];
+                    sb[right] = temp;
+
+                    left++;
+                    right--;
+                }
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+}
